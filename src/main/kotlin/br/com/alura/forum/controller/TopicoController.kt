5f0df01f2a1667ceabc211756770c8a4c31284a4
Valber Carreiro <valber.carreiro@m4u.com.br>
2021-12-14 16:19:06 -0300

    criando recursos

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,12 +1,16 @@
 package br.com.alura.forum.controller
 
+import br.com.alura.forum.dto.AlterarTopicoDTO
 import br.com.alura.forum.dto.TopicoDTO
 import br.com.alura.forum.dto.TopicoResponse
 import br.com.alura.forum.model.Curso
 import br.com.alura.forum.model.Topico
 import br.com.alura.forum.model.Usuario
 import br.com.alura.forum.service.TopicoService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriComponentsBuilder
 import java.util.*
 import javax.validation.Valid
 
@@ -25,7 +29,23 @@ class TopicoController(private val service: TopicoService) {
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody @Valid dto: TopicoDTO) {
-        service.cadastrar(dto)
+    fun cadastrar(@RequestBody @Valid dto: TopicoDTO,
+                    uriBuilder: UriComponentsBuilder): ResponseEntity<TopicoResponse> {
+        val topicoResponse = service.cadastrar(dto)
+        val uri = uriBuilder.path("/topicos/${topicoResponse.id}").build().toUri()
+        return ResponseEntity.created(uri).body(topicoResponse)
+    }
+
+    @PutMapping("/{id}")
+    fun alterar(@PathVariable id: Long,
+                @RequestBody @Valid dto: AlterarTopicoDTO): ResponseEntity<TopicoResponse> {
+        val topicoResponse = service.alterar(id, dto)
+        return ResponseEntity.ok().body(topicoResponse)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deletar(@PathVariable id: Long) {
+        service.deletar(id)
     }
 }
--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,5 +1,6 @@
 package br.com.alura.forum.service
 
+import br.com.alura.forum.dto.AlterarTopicoDTO
 import br.com.alura.forum.dto.TopicoDTO
 import br.com.alura.forum.dto.TopicoResponse
 import br.com.alura.forum.mapper.Mapper
@@ -31,9 +32,40 @@ class TopicoService(
         return topicoResponseMapper.map(topico)
     }
 
-    fun cadastrar(dto: TopicoDTO) {
+    fun cadastrar(dto: TopicoDTO): TopicoResponse {
         val topico = topicoDTOMapper.map(dto)
         topico.id = topicos.size.toLong() + 1
         topicos = topicos.plus(topico)
+
+        return topicoResponseMapper.map(topico)
+    }
+
+    fun alterar(id: Long, dto: AlterarTopicoDTO): TopicoResponse {
+        val topico = topicos.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+
+        val topicoAlterado = Topico(
+                            id = id,
+                            titulo = dto.titulo,
+                            mensagem = dto.mensagem,
+                            autor = topico.autor,
+                            curso = topico.curso,
+                            respostas = topico.respostas,
+                            status = topico.status,
+                            dataCriacao = topico.dataCriacao
+                        )
+
+        topicos = topicos.minus(topico).plus(topicoAlterado)
+
+        return return topicoResponseMapper.map(topicoAlterado)
+    }
+
+    fun deletar(id: Long) {
+        val topico = topicos.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+
+        topicos = topicos.minus(topico)
     }
 }
