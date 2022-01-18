6b62a7255ae57dddfb4880e7191d5b6784588bb8
Valber Carreiro <valbercarreiro@gmail.com>
2022-01-18 20:14:48 -0300

    add ordenacao

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -9,6 +9,7 @@ import br.com.alura.forum.model.Usuario
 import br.com.alura.forum.service.TopicoService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -25,7 +26,7 @@ class TopicoController(private val service: TopicoService) {
     @GetMapping
     fun listar(
             @RequestParam(required = false) nomeCurso: String?,
-            @PageableDefault(size = 5) paginacao: Pageable
+            @PageableDefault(size = 5, sort = ["data_criacao"], direction = Sort.Direction.DESC) paginacao: Pageable
     ): Page<TopicoResponse> {
         return service.listar(nomeCurso, paginacao)
     }
